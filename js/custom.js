147c63c4eaae8db4d9ce454894296f8ea72ce713
Darián López <daroxs95@gmail.com>
2024-03-22 01:43:43 +0100

    Add image conversion to webp

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,22 @@
+import {onCLS, onFID, onLCP} from 'https://unpkg.com/web-vitals@3?module';
 
-  (function ($) {
-  
-  "use strict";
+function sendToAnalytics(metric) {
+    const body = JSON.stringify(metric);
+    (navigator.sendBeacon && navigator.sendBeacon('https://httpbin.org/status/200', body)) ||
+    fetch('https://httpbin.org/status/200', {body, method: 'POST', keepalive: true});
+}
+
+onCLS(sendToAnalytics);
+onFID(sendToAnalytics);
+onLCP(sendToAnalytics);
+
+(function ($) {
+
+    "use strict";
 
     // NAVBAR
-    $('.navbar-nav .nav-link').click(function(){
+    $('.navbar-nav .nav-link').click(function () {
         $(".navbar-collapse").collapse('hide');
     });
-    
-  })(window.jQuery);
+
+})(window.jQuery);
